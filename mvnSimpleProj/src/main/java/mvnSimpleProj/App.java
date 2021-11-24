0ccdadf252ea7c46c6d1f71aeeafaf9a83c42434
L00145826 <L00145826@student.lyit.ie>
2021-11-24 14:27:51 +0000

    maven  pracitical fully working with eclipse

---
+++ mvnSimpleProj/src/main/java/mvnSimpleProj/App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package mvnSimpleProj;
+
+/**
+ * @author domin
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello LYIT!");
+	}
+
+}
---
+++ mvnSimpleProj/src/test/java/mvnSimpleProj/HelloWorld_Test.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package mvnSimpleProj;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+/**
+* @author Dominick Doherty
+*
+*/
+public class HelloWorld_Test {
+@Test
+public void testMain() {
+final ByteArrayOutputStream outContent = new
+ByteArrayOutputStream();
+System.setOut(new PrintStream(outContent)); 
+//make the output go to a stream instead of
+//the console
+App.main(null); //call main in the App file
+//check if the output is "Hello LYIT!" and a new line.
+assertEquals("Hello LYIT!" + System.getProperty("line.separator"),
+outContent.toString());
+}
+}
+
