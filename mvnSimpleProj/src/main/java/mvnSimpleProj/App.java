319bfc028de5cc8588e355f7359a8d77da408f0e
LiamKenny49 <60616477+LiamKenny49@users.noreply.github.com>
2021-12-01 13:10:12 +0000

    Finished Maven Practical 3

--- mvnSimpleProj/src/main/java/mvnSimpleProj/App.java
+++ mvnSimpleProj/src/main/java/mvnSimpleProj/App.java
@@ -1,11 +1,10 @@
 /**
-
  * 
  */
 package mvnSimpleProj;
 
 /**
- * @author Jalen
+ * @author liamk
  *
  */
 public class App {
@@ -14,6 +13,7 @@ public class App {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		System.out.println("Hello LYIT");
 
 	}
--- mvnSimpleProj/src/test/java/mvnSimpleProj/HelloWorld_Test.java
+++ mvnSimpleProj/src/test/java/mvnSimpleProj/HelloWorld_Test.java
@@ -5,21 +5,23 @@ package mvnSimpleProj;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+import java.io.PrintStream; 
+import mvnSimpleProj.App;
 /**
-* @author Jalen
+* @author Ruth.Lennon
 *
 */
 public class HelloWorld_Test {
-	@Test
-	public void testMain() {
-		final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-			System.setOut(new PrintStream(outContent)); //make the output go toa stream instead of
-														//the console
-		App.main(null); //call main in the App file
-		//check if the output is "Hello LYIT!" and a new line.
-		assertEquals("Hello LYIT!" + System.getProperty("line.separator"),
-				outContent.toString());
-	}
+@Test
+public void testMain() {
+final ByteArrayOutputStream outContent = new
+ByteArrayOutputStream();
+System.setOut(new PrintStream(outContent)); //make the output go to
+//a stream instead of
+//the console
+App.main(null); //call main in the App file
+//check if the output is "Hello LYIT!" and a new line.
+assertEquals("Hello LYIT!" + System.getProperty("line.separator"),
+outContent.toString());
 }
-
+}
